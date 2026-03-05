bc9dc9adb8591ca9db9cd6603c56f6ad5a935697
Muhammad Irfan <irfanmrfn28@gmail.com>
2026-03-06 06:32:55 +0700

    Lesson 1: Added variables and withdrawal logic

---
+++ CSharpFundamentalsBank/Program.cs
@@ -0,0 +1,30 @@
+﻿// Variables
+string accountHolder = "Muhammad Irfan";
+decimal accountBalance = 1500000m;
+bool isActive = true;
+decimal withdrawAmount = 500000m;
+
+// Output & String Interpolation
+Console.WriteLine($"Welcome to the C# Bank, {accountHolder}!");
+Console.WriteLine($"Your current balance is: Rp {accountBalance}");
+
+// If/Else checks
+if (isActive)
+{
+    if (withdrawAmount <= accountBalance)
+    {
+        accountBalance -= withdrawAmount;
+        Console.WriteLine($"Withdraw completed!");
+        Console.WriteLine($"Your new balance is: Rp {accountBalance}");
+    }
+    else
+    {
+        Console.WriteLine($"Withdraw failed!");
+        Console.WriteLine($"Insufficient funds.");
+    }
+}
+else
+{
+    Console.WriteLine($"Transaction failed!");
+    Console.WriteLine($"Inactive account.");
+}
